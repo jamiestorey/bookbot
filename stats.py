a377b0bacd302dfb79f730025990f5e9436a9ef4
Jamie Storey <jamie@jamiestorey.email>
2025-09-05 15:45:30 +0100

    Add: count chars

--- stats.py
+++ stats.py
@@ -1,10 +1,10 @@
+from collections import Counter
+
 def num_words_in_book(book_text):
     words = book_text.split()
     return len(words)
 
 def num_chars_in_book(book_text):
     text = book_text.lower()
-    kewl = set(text)
-    lol = dict(kewl)
-    print(lol)
-    
+    char_count = Counter(text)
+    print(char_count)
